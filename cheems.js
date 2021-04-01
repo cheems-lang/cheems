7ac8f57568a38c31e8b94ebad5b2aa34766d61d0
Ramb Memburg <heisramby@gmail.com>
2021-04-01 12:19:26 -0600

    cheems examples

--- cheems.js
+++ cheems.js
@@ -1,9 +1,9 @@
 'use strict'
 
-import run from './tokenizer.js';
+import run from './src/tokenizer.js';
 
 (() => {
-    const sentence = '25^7/1+62';
+    const sentence = '3.141592 * (5 ^ 2)';
     const tokens = run(sentence);
     const str = tokens.join(' ');
 
