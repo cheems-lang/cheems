72f04754e33518c891a030ca6b68678437bc5b01
Ramb Memburg <heisramby@gmail.com>
2021-04-02 11:53:08 -0600

    words supported

--- cheems.js
+++ cheems.js
@@ -3,9 +3,9 @@
 import run from './src/tokenizer.js';
 
 (() => {
-    const sentence = '3.141592 * (5 ^ 2)';
+    const sentence = 'word+word2*(3 / word3)^word_4';
     const tokens = run(sentence);
     const str = tokens.join(' ');
 
-    console.log(str, '=', eval(str));
+    console.log(str);
 }).call(this);
